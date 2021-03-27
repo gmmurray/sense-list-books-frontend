8f7a2130da2f39902172046f2ef32f8cdb0ddbd8
gmmurray <trinilime123@live.com>
2021-03-27 01:37:17 -0400

    testing again

--- src/main/config/startupConfig.ts
+++ src/main/config/startupConfig.ts
@@ -9,11 +9,13 @@ type auth0Credentials = { domain: string; clientId: string; audience: string };
 export const getAuth0Credentials = (): auth0Credentials => {
   switch (process.env.NODE_ENV) {
     case envTypes.dev || envTypes.prod:
-      return {
+      const res = {
         domain: process.env.REACT_APP_AUTH0_DOMAIN || '',
         clientId: process.env.REACT_APP_AUTH0_CLIENT_ID || '',
         audience: process.env.REACT_APP_AUTH0_API_AUDIENCE || '',
       };
+      console.log(res);
+      return res;
     default:
       return { domain: '', clientId: '', audience: '' };
   }
